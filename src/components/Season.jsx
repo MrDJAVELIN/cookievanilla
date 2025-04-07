7784607762e82e0cb4d7e7ec7397fbc84bcb2464
mrdjavelin <mrdjavelin@gmail.com>
2025-04-07 13:34:14 +0300

    season links fix

--- src/components/Season.jsx
+++ src/components/Season.jsx
@@ -13,7 +13,7 @@ const Season = ({ name, icon, dateStart, dateEnd, size, link }) => {
                     <span>Конец: {dateEnd}</span>
                     <span>Размер: {size}</span>
                 </div>
-                <a href={link} target="_blank">
+                <a href={link}>
                     <EmptyButton text="Загрузить" icon={<Download />} />
                 </a>
             </div>
