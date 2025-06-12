41152152064dc2de333e372b417e54a67d0d9a01
mrdjavelin <mrdjavelin@gmail.com>
2025-06-12 21:21:50 +0300

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,34 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
     title: "Cookie Vanilla",
-    description: "Cookie Vanilla - minecraft server",
+    description:
+        "Cookie vanilla — приватный ванильный ролевой сервер Minecraft с крутыми фишками, которые не портят ванильное выживание. Сервер основан на дружеском и конечно же классическом выживании без гриферства и привата.",
+    icons: {
+        icon: "/logo.png",
+    },
+};
+
+export const metadata = {
+    title: "Cookie Vanilla",
+    description:
+        "Cookie vanilla — приватный ванильный ролевой сервер Minecraft с крутыми фишками, которые не портят ванильное выживание. Сервер основан на дружеском и конечно же классическом выживании без гриферства и привата.",
+    openGraph: {
+        title: "Cookie Vanilla",
+        description:
+            "Cookie vanilla — приватный ванильный ролевой сервер Minecraft с крутыми фишками, которые не портят ванильное выживание. Сервер основан на дружеском и конечно же классическом выживании без гриферства и привата.",
+        url: "https://cookiev.xyz",
+        siteName: "Cookie Vanilla",
+        images: [
+            {
+                url: "/logo.png",
+                width: 512,
+                height: 512,
+                alt: "Cookie Vanilla Logo",
+            },
+        ],
+        locale: "ru_RU",
+        type: "website",
+    },
     icons: {
         icon: "/logo.png",
     },
